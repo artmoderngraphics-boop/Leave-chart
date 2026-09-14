c05f88e070d9d811f1c15910dffc5f5418cbab6d
artmoderngraphics-boop <artmoderngraphics@gmail.com>
2026-09-14 15:15:55 +0530

    Add files via upload

--- google-config.js
+++ google-config.js
@@ -1,6 +1,6 @@
 // Google Apps Script backend config for Leave Chart.
 window.GOOGLE_CONFIG = {
-  "enabled": false,
+  "enabled": true,
   "scriptUrl": "https://script.google.com/macros/s/AKfycbx_jq8oAoxmF00bisGXz2vtKoH1qLpp36HL9Ed5JBN8brl1HDkZpB-iCwcWHHCfbm_6/exec",
   "secret": "L123456789VE",
   "pollSeconds": 30
