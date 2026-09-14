8edb8e26f7d67a404f510603950d45f436d4e21f
artmoderngraphics-boop <artmoderngraphics@gmail.com>
2026-09-14 15:05:41 +0530

    Disable Google Apps Script and update secret

--- google-config.js
+++ google-config.js
@@ -1,7 +1,7 @@
 // Google Apps Script backend config for Leave Chart.
 window.GOOGLE_CONFIG = {
-  "enabled": true,
+  "enabled": false,
   "scriptUrl": "https://script.google.com/macros/s/AKfycbx_jq8oAoxmF00bisGXz2vtKoH1qLpp36HL9Ed5JBN8brl1HDkZpB-iCwcWHHCfbm_6/exec",
-  "secret": "Somebody-Fuck-Me",
+  "secret": "L123456789VE",
   "pollSeconds": 30
 };
